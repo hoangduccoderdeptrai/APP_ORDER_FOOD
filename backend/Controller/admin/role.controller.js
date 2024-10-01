6dd9beb78d889e27681c49de325b6ffe2690e56a
Duymaoit <duyp6090@gmail.com>
2024-10-01 14:29:43 +0700

    config controller

---
+++ backend/Controller/admin/role.controller.js
@@ -0,0 +1,8 @@
+import { Role } from "../../Model/role.model.js";
+
+const getRoles = async (req, res) => {
+    try {
+    } catch (err) {
+        res.status(500).json({ msg: err.message });
+    }
+};
