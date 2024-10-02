b2290200d172530e1491174da0a7a055ad3ee730
Duymaoit <duyp6090@gmail.com>
2024-10-02 08:42:17 +0700

    #4 - manageme role of admin

---
+++ backend/helper/search.js
@@ -0,0 +1,20 @@
+function search(query) {
+    // Define object search
+    let objectSearch = {
+        keyword: "",
+        regex: "",
+    };
+
+    // Get keyword from query
+    objectSearch.keyword = query.keyword;
+
+    // Check if has keyword
+    if (objectSearch.keyword) {
+        // Use regex to convert
+        objectSearch.regex = new RegExp(objectSearch.keyword, "i");
+    }
+
+    return objectSearch;
+}
+
+export default search;
