21de75846f8d8748eda5c15600c3bad6c044ee29
Duymaoit <duyp6090@gmail.com>
2024-10-06 21:14:58 +0700

    #8-manage-account

--- backend/routes/admin/admin.router.js
+++ backend/routes/admin/admin.router.js
@@ -1,8 +1,13 @@
 // Import router role
 import roleRouter from "./role.router.js";
 
+// Import account router
+import accountRouter from "./account.role.js";
+
 function webInitRouterAdmin(app) {
-    app.use("/admin", roleRouter);
+    app.use("/admin/role", roleRouter);
+
+    app.use("/admin/account", accountRouter);
 }
 
 export default webInitRouterAdmin;
