6d2e686eaeb16ee686a85042adbe13d0e3d166f4
Duymaoit <duyp6090@gmail.com>
2024-11-28 23:33:44 +0700

    #40 updated- fixed searc page 11

--- backend/Controller/user/questionGemini.controller.js
+++ backend/Controller/user/questionGemini.controller.js
@@ -16,25 +16,24 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 // Calling function to ask question to the generative model
 
 // 1. Ask for the information of a restaurant - API function
-async function informationRestaurant(restaurantName, borough) {
+async function informationRestaurant(listRestaurantName) {
     try {
         // Find condition
         let find = {
             status: "active",
         };
 
-        // Search the restaurant by name and borough
-        const objectSearchRestaurant = search(restaurantName);
-        const objectSearchBorough = search(borough);
-        if (objectSearchRestaurant.regex) {
-            find.name = objectSearchRestaurant.regex;
-        }
-        if (objectSearchBorough.regex) {
-            find["address.borough"] = objectSearchBorough.regex;
+        // Search the restaurant by name
+        let newListRestaurantName = listRestaurantName.map((restaurantName) => {
+            return search(restaurantName).regex;
+        });
+
+        if (newListRestaurantName && newListRestaurantName.length > 0) {
+            find["name"] = { $in: newListRestaurantName };
         }
 
         // Find the restaurant
-        const restaurants = await Restaurant.find(find).select(
+        let restaurants = await Restaurant.find(find).select(
             "name address phone time_open time_close starMedium"
         );
 
@@ -42,8 +41,35 @@ async function informationRestaurant(restaurantName, borough) {
         if (!restaurants || restaurants.length === 0) {
             return "Không tìm thấy nhà hàng";
         }
+        // Get the hot food of the restaurant
+        const promiseRestaurant = restaurants.map(async (restaurant) => {
+            // Convert the restaurant to object
+            restaurant = restaurant.toObject();
+
+            // Get id of the restaurant
+            const restaurantId = restaurant._id;
+
+            // Find the hot food of the restaurant
+            const hotFoods = await MenuItem.find({
+                restaurantId: restaurantId,
+            })
+                .sort({ quantitySolded: -1, starMedium: -1 })
+                .limit(3)
+                .select("title price");
+
+            return {
+                ...restaurant,
+                hotFoods: hotFoods,
+            };
+        });
+
+        // Wait for all promise
+        restaurants = await Promise.all(promiseRestaurant);
+
+        // Return the restaurant
         return restaurants;
     } catch (error) {
+        console.log(error.message);
         return "Đã xảy ra lỗi khi tìm kiếm nhà hàng. Vui lòng thử lại sau.";
     }
 }
@@ -56,20 +82,20 @@ const informationRestaurantDeclaration = {
         type: "object",
         description: "Tìm kiếm thông tin của một nhà hàng",
         properties: {
-            restaurantName: {
-                type: "string",
-                description: "Tên nhà hàng",
-            },
-            borough: {
-                type: "string",
-                description: "Quận",
+            listRestaurantName: {
+                type: "array",
+                description: "Danh sách tên nhà hàng khách hàng muốn tìm kiếm",
+                items: {
+                    type: "string",
+                    description: "Tên của từng nhà hàng",
+                },
             },
         },
-        required: ["restaurantName"],
+        required: ["listRestaurantName"],
     },
 };
 
-// 2 Recommend restaurant fot user - API function
+// 2. Recommend restaurant fot user - API function
 async function recommendedRestaurant(borough, street, rating, time_open, time_close, categories) {
     try {
         // Find condition
@@ -80,18 +106,16 @@ async function recommendedRestaurant(borough, street, rating, time_open, time_cl
         // Recommend restaurant by information that user give
         const objectSearchBorough = search(borough);
         const objectSearchStreet = search(street);
-        const starMedium = parseInt(rating);
         const objectSearchTimeOpen = search(time_open);
         const objectSearchTimeClose = search(time_close);
-
         if (objectSearchBorough.regex) {
             find["address.borough"] = objectSearchBorough.regex;
         }
         if (objectSearchStreet.regex) {
             find["address.street"] = objectSearchStreet.regex;
         }
-        if (starMedium) {
-            find["starMedium"] = { $gte: starMedium };
+        if (rating) {
+            find["starMedium"] = { $gte: rating };
         }
         if (objectSearchTimeOpen.regex) {
             find.time_open = objectSearchTimeOpen.regex;
@@ -101,7 +125,7 @@ async function recommendedRestaurant(borough, street, rating, time_open, time_cl
         }
 
         // Find the restaurant
-        const restaurants = await Restaurant.find(find).select("name address");
+        const restaurants = await Restaurant.find(find).select("name address").limit(5);
 
         if (categories && categories.length > 0) {
             // New categories array
@@ -111,6 +135,9 @@ async function recommendedRestaurant(borough, street, rating, time_open, time_cl
 
             // Find the food of the restaurant
             const promiseRestaurant = restaurants.map(async (restaurant) => {
+                // Convert the restaurant to object
+                restaurant = restaurant.toObject();
+
                 // Get id of the restaurant
                 const restaurantId = restaurant._id;
 
@@ -118,10 +145,13 @@ async function recommendedRestaurant(borough, street, rating, time_open, time_cl
                 const foods = await MenuItem.find({
                     restaurantId: restaurantId,
                     category: { $in: newCategories },
-                }).select("title");
+                })
+                    .sort({ starMedium: -1 })
+                    .limit(3)
+                    .select("title");
 
                 return {
-                    ...restaurant._doc,
+                    ...restaurant,
                     foods: foods,
                 };
             });
@@ -137,7 +167,8 @@ async function recommendedRestaurant(borough, street, rating, time_open, time_cl
 
         return restaurants;
     } catch (error) {
-        return "Đã xảy ra lỗi khi tìm kiếm nhà hàng. Vui lòng thử lại sau.";
+        console.log(error.message);
+        return "Đã xảy ra lỗi khi tìm kiếm nhà hàng. Vui lòng thử lại sau!";
     }
 }
 
@@ -157,7 +188,7 @@ const recommendedRestaurantDeclaration = {
             },
             street: {
                 type: "string",
-                description: "Đường",
+                description: "Đường phố của nhà hàng",
             },
             rating: {
                 type: "number",
@@ -173,7 +204,7 @@ const recommendedRestaurantDeclaration = {
             },
             categories: {
                 type: "array",
-                description: "Danh mục món ăn của nhà hàng",
+                description: "Danh sách loại món ăn",
                 items: {
                     type: "string",
                     description: "Tên loại món ăn",
@@ -183,10 +214,199 @@ const recommendedRestaurantDeclaration = {
     },
 };
 
+// 3. Find specialty food - API function
+async function specialtyFood() {
+    try {
+        // Find the specialty food
+        const specialtyFoods = await SpecialtyFood.find({}).limit(5).select("name");
+
+        // If the specialty food is not found
+        if (!specialtyFoods || specialtyFoods.length === 0) {
+            return "Không tìm thấy món ăn đặc biệt";
+        }
+
+        return specialtyFoods;
+    } catch (error) {
+        console.log(error.message);
+        return "Đã xảy ra lỗi khi tìm kiếm món ăn đặc biệt. Vui lòng thử lại sau!";
+    }
+}
+
+// 3.1 Create fuction declarations for the specialty food
+const specialtyFoodDeclaration = {
+    name: "specialtyFood",
+    description: "Tìm kiếm món ăn đặc biệt của trang web Yummy",
+    parameters: {
+        type: "object",
+        description: "Tìm kiếm món ăn đặc biệt của trang web Yummy",
+        properties: {
+            name: {
+                type: "string",
+                description: "Tên món ăn đặc biệt",
+            },
+        },
+    },
+};
+
+// 4. Recommended food by name - API function
+async function recommendedFoods(
+    name,
+    description,
+    minPrice = 0,
+    maxprice = 1e9,
+    categories,
+    discount,
+    starMedium
+) {
+    try {
+        // Find condition
+        let find = {
+            isAvailable: true,
+        };
+
+        // Search information of list food
+        const objectSearchName = search(name);
+        const objectSearchDescription = search(description);
+        const newCategories = categories.map((category) => {
+            return search(category).regex;
+        });
+        if (objectSearchName.regex) {
+            find.title = objectSearchName.regex;
+        }
+        if (objectSearchDescription.regex) {
+            find.description = objectSearchDescription.regex;
+        }
+        if (starMedium) {
+            find.starMedium = { $gte: starMedium };
+        }
+        if (categories && categories.length > 0) {
+            find.category = { $in: newCategories };
+        }
+        if (discount) {
+            find.discount = { $gte: discount };
+        }
+        find.price = { $gte: minPrice, $lte: maxprice };
+
+        // Find the food
+        const foods = await MenuItem.find(find)
+            .sort({ starMedium: -1 })
+            .limit(5)
+            .select("title price category discount starMedium category");
+
+        // If the food is not found
+        if (!foods || foods.length === 0) {
+            return "Không tìm thấy món ăn";
+        }
+
+        return foods;
+    } catch (error) {
+        console.log(error.message);
+        return "Đã xảy ra lỗi khi tìm kiếm món ăn. Vui lòng thử lại sau!";
+    }
+}
+
+// 4.1 Create fuction declarations for the recommended food by name
+const recommendedFoodsDeclaration = {
+    name: "recommendedFoods",
+    description:
+        "Gợi ý các món ăn dựa vào mô tả của khách hàng như tên, mô tả, giá, loại món ăn, giảm giá, đánh giá",
+    parameters: {
+        type: "object",
+        description: "Gợi ý các món ăn dựa vào tên, mô tả, giá, loại món ăn, giảm giá, đánh giá",
+        properties: {
+            name: {
+                type: "string",
+                description: "Tên món ăn",
+            },
+            description: {
+                type: "string",
+                description: "Mô tả chi tiết món ăn",
+            },
+            minPrice: {
+                type: "number",
+                description: "Giá thấp nhất của món ăn",
+            },
+            maxPrice: {
+                type: "number",
+                description: "Giá cao nhất của món ăn",
+            },
+            categories: {
+                type: "array",
+                description: "Danh sách loại món ăn",
+                items: {
+                    type: "string",
+                    description: "Tên loại món ăn",
+                },
+            },
+            discount: {
+                type: "number",
+                description: "Giảm giá của món ăn",
+            },
+            starMedium: {
+                type: "number",
+                description: "Đánh giá sao trung bình của món ăn",
+            },
+        },
+    },
+};
+
+// 5. Find information of a foods - API function
+async function informationFoods(listFoodName) {
+    try {
+        // Find condition
+        let find = {
+            isAvailable: true,
+        };
+
+        // Search information of list food
+        const newListFoodName = listFoodName.map((foodName) => {
+            return search(foodName).regex;
+        });
+
+        if (newListFoodName && newListFoodName.length > 0) {
+            find.title = { $in: newListFoodName };
+        }
+
+        // Find the food
+        const foods = await MenuItem.find(find).select("title price category discount starMedium");
+
+        // If the food is not found
+        if (!foods || foods.length === 0) {
+            return "Không tìm thấy món ăn";
+        }
+
+        return foods;
+    } catch (error) {
+        console.log(error.message);
+        return "Đã xảy ra lỗi khi tìm kiếm món ăn. Vui lòng thử lại sau!";
+    }
+}
+
+// 5.1 Create fuction declarations for the information of a foods
+const informationFoodsDeclaration = {
+    name: "informationOfFoods",
+    description: "Tìm kiếm thông tin của một món ăn dựa vào tên",
+    parameters: {
+        type: "object",
+        description: "Tìm kiếm thông tin của một món ăn",
+        properties: {
+            listFoodName: {
+                type: "array",
+                description: "Danh sách tên món ăn khách hàng muốn tìm kiếm",
+                items: {
+                    type: "string",
+                    description: "Tên của từng món ăn",
+                },
+            },
+        },
+        required: ["listFoodName"],
+    },
+};
+
 // Create list of function
 const functions = {
-    informationOfRestaurant: ({ restaurantName, borough }) => {
-        return informationRestaurant(restaurantName, borough);
+    informationOfRestaurant: ({ listRestaurantName }) => {
+        return informationRestaurant(listRestaurantName);
     },
     recommendRestaurantForUser: ({
         borough,
@@ -198,6 +418,31 @@ const functions = {
     }) => {
         return recommendedRestaurant(borough, street, rating, time_open, time_close, categories);
     },
+    specialtyFood: () => {
+        return specialtyFood();
+    },
+    recommendedFoods: ({
+        name,
+        description,
+        minPrice,
+        maxPrice,
+        categories,
+        discount,
+        starMedium,
+    }) => {
+        return recommendedFoods(
+            name,
+            description,
+            minPrice,
+            maxPrice,
+            categories,
+            discount,
+            starMedium
+        );
+    },
+    informationOfFoods: ({ listFoodName }) => {
+        return informationFoods(listFoodName);
+    },
 };
 
 // Create a new instance of the GoogleGenerativeAI
@@ -208,13 +453,19 @@ const generatetiveModel = genAI.getGenerativeModel({
     systemInstruction:
         "Bạn là một chatbot của ứng dụng Yummy. Nhiệm vụ của bạn là hỗ trợ người dùng tìm hiểu về thông tin của website Yummy. Website Yummy là trang web cho phép mọi người đặt và bán đồ ăn, thức uống. Những thông tin cơ bản mà người dùng cần chủ yếu là các thông tin của món ăn và nhà hàng, đơn đặt hàng của họ.Bạn có thể giúp tôi không?",
     tools: {
-        functionDeclarations: [informationRestaurantDeclaration, recommendedRestaurantDeclaration],
+        functionDeclarations: [
+            informationRestaurantDeclaration,
+            recommendedRestaurantDeclaration,
+            specialtyFoodDeclaration,
+            recommendedFoodsDeclaration,
+            informationFoodsDeclaration,
+        ],
     },
 });
 
 const questionGemini = async (req, res) => {
     // Get hisstory from the request body
-    const historyChat = req.body.historyChat;
+    const historyChat = req.body.history;
     const newQuestion = req.body.question;
 
     // Start chat with the generative model
@@ -236,14 +487,18 @@ const questionGemini = async (req, res) => {
 
         // Call the function
         const apiResponse = await functions[callingFunction.name](callingFunction.args);
+        console.log(apiResponse);
+
+        // Ensure apiResponse is properly formatted
+        const functionResponse = {
+            name: callingFunction.name,
+            response: { items: apiResponse },
+        };
 
         // Reget the response from the generative model
         const result2 = await chat.sendMessage([
             {
-                functionResponse: {
-                    name: callingFunction.name,
-                    response: apiResponse,
-                },
+                functionResponse: functionResponse,
             },
         ]);
 
