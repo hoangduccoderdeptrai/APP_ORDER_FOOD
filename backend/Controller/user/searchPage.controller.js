8d53d4628f3886797b6b546368e655876b685573
Duymaoit <duyp6090@gmail.com>
2024-12-17 22:30:18 +0700

    fix bug search page

--- backend/Controller/user/searchPage.controller.js
+++ backend/Controller/user/searchPage.controller.js
@@ -75,9 +75,12 @@ const searchPage = async (req, res) => {
             // Create object search nameFood
             const objectSearchName = search(nameFood);
 
+            const newCategories = categories.map((category) => {
+                return search(category).regex;
+            });
             if (categories && categories.length > 0) {
                 findFood["category"] = {
-                    $in: categories,
+                    $in: newCategories,
                 };
             }
 
@@ -87,21 +90,26 @@ const searchPage = async (req, res) => {
 
             // Find food
             listFood = await MenuItem.find(findFood);
+
+            // Check have list food
+            if (!listFood || listFood.length === 0) {
+                return res.status(200).json({
+                    msg: "Not found food",
+                });
+            }
         }
 
         // Check have list food
-        if (listFood.length > 0) {
-            // Get list id restaurant
-            let listIdRestaurant = listFood.map((food) => food.restaurantId.toString());
+        // Get list id restaurant
+        let listIdRestaurant = listFood.map((food) => food.restaurantId.toString());
 
-            // Filter unique id restaurant
-            listIdRestaurant = [...new Set(listIdRestaurant)];
+        // Filter unique id restaurant
+        listIdRestaurant = [...new Set(listIdRestaurant)];
 
-            // Update find restaurant to save id restaurant
-            findRestaurant["_id"] = {
-                $in: listIdRestaurant,
-            };
-        }
+        // Update find restaurant to save id restaurant
+        findRestaurant["_id"] = {
+            $in: listIdRestaurant,
+        };
 
         // Object pagination
         const numRestaurants = await Restaurant.countDocuments(findRestaurant);
